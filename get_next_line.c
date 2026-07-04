612a10e128f1b6d3edf972448a5fadd5235655e4
Ilanna Freire <ifreire@c3r13p3.42sp.org.br>
2026-07-04 11:59:11 -0300

    refactor: split ft_append and get_next_line to comply with 25-line norm

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ifreire <ifreire@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/30 23:21:36 by ifreire           #+#    #+#             */
-/*   Updated: 2026/07/01 00:12:39 by ifreire          ###   ########.fr       */
+/*   Updated: 2026/07/04 11:49:32 by ifreire          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,45 +71,86 @@ static char	*cut(char **str)
 	return (ret);
 }
 
-static void	ft_append(char **str, char *buff)
+static void	ft_grow_cap(char **str, size_t str_len,
+	size_t *cap, size_t new_cap)
 {
-	char	*aux;
+	char	*new_str;
 
-	if (!*str)
-		*str = ft_strdup(buff);
-	else
+	new_str = malloc(new_cap);
+	if (!new_str)
+		return ;
+	if (*str)
 	{
-		aux = ft_strjoin(*str, buff);
-		ft_freethis(str);
-		*str = aux;
+		ft_strlcpy(new_str, *str, str_len + 1);
+		free(*str);
 	}
+	else
+		new_str[0] = '\0';
+	*str = new_str;
+	*cap = new_cap;
 }
 
-char	*get_next_line(int fd)
+static void	ft_append(char **str, char *buff, size_t *cap)
 {
-	char		*buff;
-	int			size;
-	static char	*str;
+	size_t	str_len;
+	size_t	buff_len;
+	size_t	needed;
+	size_t	new_cap;
 
-	if (fd < 0 || BUFFER_SIZE < 1)
-		return (NULL);
+	str_len = ft_strlen(*str);
+	buff_len = ft_strlen(buff);
+	needed = str_len + buff_len + 1;
+	if (needed > *cap)
+	{
+		new_cap = *cap * 2;
+		if (new_cap < needed)
+			new_cap = needed * 2;
+		ft_grow_cap(str, str_len, cap, new_cap);
+		if (needed > *cap)
+			return ;
+	}
+	ft_strlcpy(*str + str_len, buff, buff_len + 1);
+}
+
+static int	ft_read_loop(int fd, char **str, size_t *cap)
+{
+	char	*buff;
+	int		size;
+
 	buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buff)
-		return (NULL);
+		return (-1);
 	size = read(fd, buff, BUFFER_SIZE);
 	while (size > 0)
 	{
 		buff[size] = '\0';
-		ft_append(&str, buff);
-		if (ft_strchr(str, '\n'))
+		ft_append(str, buff, cap);
+		if (ft_strchr(buff, '\n'))
 			break ;
 		size = read(fd, buff, BUFFER_SIZE);
 	}
 	ft_freethis(&buff);
-	if (size < 0)
-	{
-		ft_freethis(&str);
+	return (size);
+}
+
+char	*get_next_line(int fd)
+{
+	int			size;
+	static char	*str;
+	static size_t	cap;
+
+	if (fd < 0 || BUFFER_SIZE < 1)
 		return (NULL);
+	if (!ft_strchr(str, '\n'))
+	{
+		size = ft_read_loop(fd, &str, &cap);
+		if (size < 0)
+		{
+			ft_freethis(&str);
+			cap = 0;
+			return (NULL);
+		}
 	}
+	cap = 0;
 	return (cut(&str));
 }
--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ifreire <ifreire@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/04 00:00:00 by ifreire           #+#    #+#             */
-/*   Updated: 2026/07/04 08:28:52 by ifreire          ###   ########.fr       */
+/*   Updated: 2026/07/04 11:49:51 by ifreire          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,45 +71,86 @@ static char	*cut(char **str)
 	return (ret);
 }
 
-static void	ft_append(char **str, char *buff)
+static void	ft_grow_cap(char **str, size_t str_len,
+	size_t *cap, size_t new_cap)
 {
-	char	*aux;
+	char	*new_str;
 
-	if (!*str)
-		*str = ft_strdup(buff);
-	else
+	new_str = malloc(new_cap);
+	if (!new_str)
+		return ;
+	if (*str)
 	{
-		aux = ft_strjoin(*str, buff);
-		ft_freethis(str);
-		*str = aux;
+		ft_strlcpy(new_str, *str, str_len + 1);
+		free(*str);
 	}
+	else
+		new_str[0] = '\0';
+	*str = new_str;
+	*cap = new_cap;
 }
 
-char	*get_next_line(int fd)
+static void	ft_append(char **str, char *buff, size_t *cap)
 {
-	char		*buff;
-	int			size;
-	static char	*str[OPEN_MAX];
+	size_t	str_len;
+	size_t	buff_len;
+	size_t	needed;
+	size_t	new_cap;
 
-	if (fd < 0 || fd >= OPEN_MAX || BUFFER_SIZE < 1)
-		return (NULL);
+	str_len = ft_strlen(*str);
+	buff_len = ft_strlen(buff);
+	needed = str_len + buff_len + 1;
+	if (needed > *cap)
+	{
+		new_cap = *cap * 2;
+		if (new_cap < needed)
+			new_cap = needed * 2;
+		ft_grow_cap(str, str_len, cap, new_cap);
+		if (needed > *cap)
+			return ;
+	}
+	ft_strlcpy(*str + str_len, buff, buff_len + 1);
+}
+
+static int	ft_read_loop(int fd, char **str, size_t *cap)
+{
+	char	*buff;
+	int		size;
+
 	buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buff)
-		return (NULL);
+		return (-1);
 	size = read(fd, buff, BUFFER_SIZE);
 	while (size > 0)
 	{
 		buff[size] = '\0';
-		ft_append(&str[fd], buff);
-		if (ft_strchr(str[fd], '\n'))
+		ft_append(str, buff, cap);
+		if (ft_strchr(buff, '\n'))
 			break ;
 		size = read(fd, buff, BUFFER_SIZE);
 	}
 	ft_freethis(&buff);
-	if (size < 0)
-	{
-		ft_freethis(&str[fd]);
+	return (size);
+}
+
+char	*get_next_line(int fd)
+{
+	int				size;
+	static char		*str[OPEN_MAX];
+	static size_t	cap[OPEN_MAX];
+
+	if (fd < 0 || fd >= OPEN_MAX || BUFFER_SIZE < 1)
 		return (NULL);
+	if (!ft_strchr(str[fd], '\n'))
+	{
+		size = ft_read_loop(fd, &str[fd], &cap[fd]);
+		if (size < 0)
+		{
+			ft_freethis(&str[fd]);
+			cap[fd] = 0;
+			return (NULL);
+		}
 	}
+	cap[fd] = 0;
 	return (cut(&str[fd]));
 }
